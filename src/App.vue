914b02d95c8545f57eb073a51a64655f8b980a3e
benadidafouzia <benadidafouzia@gmail.com>
2022-11-08 16:33:05 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+  <NavBar/>
+    <v-main>
+      <router-view/>
+    </v-main>
+  <Footer/>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
+
+export default Vue.extend({
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+  components: {
+    NavBar,
+    Footer
+  }
+});
+</script>
