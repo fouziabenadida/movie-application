fc4a206b0445bf029040d6af59f2f74e24da89bb
benadidafouzia <benadidafouzia@gmail.com>
2022-11-09 17:00:47 +0300

    movie details done

---
+++ src/components/Cast.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+name: 'CastView'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ImageView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+name: 'ImageView'
+}
+</script>
+
+<style>
+
+</style>
