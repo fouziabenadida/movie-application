d1915b980c87d9b084607febf6b9fe7ae392bbc1
benadidafouzia <benadidafouzia@gmail.com>
2022-11-09 11:02:21 +0300

    third commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,17 @@
 <template>
-  <div></div>
+ <PopularMovie/>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
+  import PopularMovie from '../components/PopularMovie.vue'
 
 
   export default Vue.extend({
     name: 'HomeView',
 
     components: {
-
+  PopularMovie
     },
   })
 </script>
