602d9990b1eb8eb0630d62d58f4818e623404edf
benadidafouzia <benadidafouzia@gmail.com>
2022-11-09 11:01:56 +0300

    third commit

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,39 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+props: {
+    movie: {
+        required: true
+    },
+    genres: {
+        required: true,
+    }
+},
+computed: {
+    posterPath() {
+        return "https://image.tmdb.org/t/p/w500/" + this.movie.poster_path
+    }
+},
+methods: {
+    genreTypeName(genraId, index) {
+      for(const item of this.genres) {
+        if(item.id == genraId) {
+          if(this.movie.genre_ids.length - 1 == index){
+            return item.name ;
+          }else {
+            return item.name + ",";
+          }
+          
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
