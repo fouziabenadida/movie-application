ad1ebb9aa4996b52f25363609bd233c8a3a19385
benadidafouzia <benadidafouzia@gmail.com>
2022-11-09 13:58:23 +0300

    upcomming movie

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,22 @@
 <template>
+  <div>
  <PopularMovie/>
+ <UpCommingMovie/>
+</div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
   import PopularMovie from '../components/PopularMovie.vue'
+  import UpCommingMovie from '@/components/UpCommingMovie.vue';
 
 
   export default Vue.extend({
     name: 'HomeView',
 
     components: {
-  PopularMovie
+  PopularMovie,
+  UpCommingMovie
     },
   })
 </script>
